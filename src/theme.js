a3f98f225a7026832cd6e6ab4fbbbfc4af68301a
Your Name <jxtsly@gmail.com>
2024-02-12 11:44:17 +0100

    typography

--- src/theme.js
+++ src/theme.js
@@ -121,29 +121,73 @@ export const tokens = (mode) => ({
 });
 
 // mui theme settings
-export const themeSettings = (mode)=> {
-    const colors = tokens(mode);
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
 
-    return{
-        palette:{
-            mode: mode,
-            ...(mode === 'dark'
-            ?{
-                primary:{
-                    main:colors.primary[500],
-                },
-                secondary:{
-                    main:colors.greenAccent[500],
-                },
-                neutral:{
-                    dark:colors.grey[700],
-                    main:colors.grey[500],
-                    light:colors.grey[100]
-                },
-                background:{
-                    default:colors.primary[500],
-                }
-            })
-        }
-    }
-}
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
