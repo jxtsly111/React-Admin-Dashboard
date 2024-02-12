d4a573cfd65ddd199dd459248680f8745da0956e
Your Name <jxtsly@gmail.com>
2024-02-12 19:03:34 +0100

    icon button

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -13,9 +13,21 @@ const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  return (<Box display ="flex" justifyContent= >
-
-  </Box>);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Topbar;
